523fb7d0b5451e7c6107127756d5349df92af798
Sebastian Seilund <sebastian@seilund.com>
2013-10-23 16:30:35 -0700

    added files

---
+++ src/js/tool.js
@@ -0,0 +1,38 @@
+module.exports = Em.Component.extend(require('ember-tooltip').Tooltipable, {
+    template: require('../templates/tool'),
+    
+    classNameBindings: [':tool', 'iconClassName', 'large'],
+
+    tip: Em.I18n.tProperty('tip'),
+    text: Em.I18n.tProperty('text'),
+    tipPosition: 'top',
+
+    icon: null,
+    iconClassName: function() {
+        return 'tool-'+this.get('icon').replace('/', '-');
+    }.property('icon'),
+    
+    action: null,
+    target: null,
+    
+    click: function(e) {
+        var target = this.get('target'),
+            action = this.get('action'),
+            actionContext = this.get('actionContext');
+        if (_.isString(target)) {
+            target = Ember.Handlebars.get(this, target);
+        }
+        if (!target) {
+            return;
+        }
+        e.stopPropagation();
+        if (typeof action === 'function') {
+            return action.call(target, actionContext);
+        } else if (typeof target.send === 'function') {
+            return target.send(action, actionContext);
+        } else {
+            Ember.assert("The action '" + action + "' did not exist on " + target, typeof target[action] === 'function');
+            return target[action](actionContext);
+        }
+    }
+});
